c63fd43eaeebd4153bd9a54f58575340c6fb9db1
dylandomingo57@ <dylandomingo57@gmail.com>
2023-12-20 17:30:02 -1000

    bruH !!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
-import Grid from './components/grid.js';
-import DirectionButton from './components/directionButton';
+import {useState, useEffect} from "react";
+import Button from './components/button';
+import React from 'react';
 
 function App() {
+
+  const choiceDictionary = {
+    0: "Rock",
+    1: "Paper",
+    2: "Scissors"
+  }
+
+  const [playerChoice, setPlayerChoice] = useState("");
+  const [computerChoice, setComputerChoice] = useState("");
+  const [win, setWin] = useState(null);
+
+  function decideWinner() {
+    if (playerChoice == "Rock") { 
+      if (computerChoice == "Paper") { setWin(false) }
+      else if (computerChoice == "Scissors") { setWin(true) }
+    }
+    else if (playerChoice == "Paper") {
+      if (computerChoice == "Rock") { setWin(true) }
+      else if (computerChoice == "Scissors") { setWin(false) }
+    }
+    else if (playerChoice == "Scissors") {
+      if (computerChoice == "Rock") { setWin(true) }
+      else if (computerChoice == "Paper") { setWin(false) }
+    }
+    else if (playerChoice == computerChoice) {
+      setWin(null)
+    }
+  }
+
+  function handleClick(choice) {
+    //computer choice
+    setComputerChoice(choiceDictionary[Math.floor(Math.random() * 2)])
+
+    //plr choice
+    setPlayerChoice(choice)
+    decideWinner()
+
+    // if (win) {
+      
+    // }
+    // else if (!win) {
+
+    // }
+    // else {
+
+    // }
+  }
+
   return (
     <div class="container">
       <div class="titleDiv">
-        <p class="paragraph"> SNAKE GAME </p>
+        <p class="paragraph"> ROCK PAPAH SCISSAHSSSSSSSS </p>
       </div>
-      <div class = "gameDiv">
-        <Grid/>
+      <div class = "opponentChoice">
+        <p> {computerChoice} </p>
+      </div>
+      <div>
+        <p> </p>
       </div>
       <div class="controlButtons">
-        <DirectionButton direction="up" text="↑"/>
-        <br></br>
-        <DirectionButton direction="left" text="←"/>
-        <DirectionButton direction="down" text="↓"/>
-        <DirectionButton direction="right" text="→"/>
+        <Button text="Rock" onButtonClick={handleClick("Rock")}/>
+        <Button text="Paper" onButtonClick={handleClick("Paper")}/>
+        <Button text="Scissors" onButtonClick={handleClick("Scissors")}/>
       </div>
     </div>
   );
---
+++ src/components/button.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const Button = (props) => {
+    return (
+        <button class="btn" onClick={props.onButtonClick}>
+            {props.text}
+        </button>
+    )
+}
+
+export default Button;
